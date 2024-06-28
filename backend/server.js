41342222d4d6e7b003b9461f6a8771c38158a4e0
Yug2099 <yug.g03@gmail.com>
2024-06-28 18:35:26 +0530

    cors origin

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ const server = app.listen(PORT, console.log(`Server started at port ${PORT}`));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://my-group-chat.vercel.app/",
+    origin: "https://my-group-chat.vercel.app",
   },
 });
 
